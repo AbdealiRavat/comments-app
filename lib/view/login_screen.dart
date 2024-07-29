bfb889b9a4f8f84668b4c561e8cd9c69c563b57a
Abdeali Ravat <rawatabdeali22@gmail.com>
2024-07-29 19:06:10 +0530

    login screen UI Completed, fonts added

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -13,13 +13,18 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   LoginController login = Get.put(LoginController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: Text(
         'Comments',
-        style: TextStyle(color: Color(0xff0c54be)),
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.bold,
+            color: const Color(0xff0c54be),
+            fontSize: 24.sp),
       )),
       body: Padding(
         padding: EdgeInsets.symmetric(
@@ -28,9 +33,26 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            commonTextField(login.nameController.value, 'Name', Colors.black, false, false),
-            commonTextField(login.emailController.value, 'Email', Colors.black, false, false),
-            commonTextField(login.passwordController.value, 'Password', Colors.black, false, false),
+            Spacer(),
+            commonTextField(login.emailController.value, 'Email', Colors.black, true, false),
+            commonTextField(login.passwordController.value, 'Password', Colors.black, true, true),
+            Spacer(),
+            buttonWidget(() {}, 'Login'),
+            SizedBox(height: 10.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'New here?',
+                  style: TextStyle(fontSize: 14.sp, color: Colors.black),
+                ),
+                Text(
+                  ' Signup',
+                  style: TextStyle(fontSize: 14.sp, color: const Color(0xff0c54be)),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.h),
           ],
         ),
       ),
@@ -80,4 +102,23 @@ Widget commonTextField(TextEditingController controller, String hintText, Color 
           hintStyle: TextStyle(color: Colors.grey.shade400, fontSize: 16)),
     ),
   );
+}
+
+Widget buttonWidget(void Function() onTap, String text) {
+  return ElevatedButton(
+      style: ButtonStyle(
+        shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.r))),
+        padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 90.w, vertical: 12.h)),
+        backgroundColor: MaterialStatePropertyAll(Color(0xff0c54be)),
+      ),
+      onPressed: onTap,
+      child: Text(
+        text,
+        style: TextStyle(
+            fontSize: 16.sp,
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.bold),
+      ));
 }
